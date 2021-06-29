21502b6c65be994f3aaab2cdbcc472f16286db88
Ake <akhe@grodansparadis.com>
2021-06-29 10:32:01 +0200

    Changed buildsystem to cmake

---
+++ src/version.h
@@ -0,0 +1,33 @@
+// version.h
+//
+// Build instructions for VSCP Works.
+//
+// Copyright (c) 2000-2021 Åke Hedman, the VSCP Project
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+#define MAJOR_VERSION       15
+#define MINOR_VERSION       0
+#define RELEASE_VERSION     0
+#define BUILD_VERSION       0
+
+#define DISPLAY_VERSION "15.0.0.0"
+#define COPYRIGHT       "Copyright (c) 2000-2021 Ake Hedman, the VSCP Project"
+#define COPYRIGHT_HTML  "Copyright (c) 2000-2021 Ake Hedman, the VSCP Project"
